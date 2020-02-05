9ca452c8f29acd7f4d9731b9df55dc534ad870ed
Najwab <inajjwa2@gmail.com>
2020-02-05 11:50:08 +0300

    complete

---
+++ README.md
@@ -0,0 +1,40 @@
+#List technologies used: 
+* HTML 
+* CSS 
+* Javascript  
+* DOM 
+
+
+#Link to wireframes and user stories.
+[wireframe](https://wireframe.cc/PMR30q)
+
+
+####List unsolved problems which would be fixed in future iterations.
+* Solve who start first 
+* And make my code more organized and not repeatable 
+
+
+#Describe how you solved for the winner.
+
+the function started by an if statement that checks the first row of the zones by checking array index of the **first row [0,1,2]** it will check if index[0] inner text equal to index [1] **AND** IF index[1] equal to index[2].And if it's true it will check if the inner text equals X then it will print player one name Win else if it O it will print player two name win!. and will check second, third row, first, second, third column, and first,second cross as well
+
+and I initialize two global variables at the beginning the first one called counter and the second one gameover its boolean variable and I assigned to it **false**. the counter variable will count the number of clicks and gameover variable value will be changed to true in each if condition, means if this condition true it will change the game over variable value to true
+
+In the end, there is else if to check tie situation if counter equal to 9 AND game over is false it will print it's a Tie.  
+
+
+##Describe how some of your favorite functions work
+
+`function whoWin(who) {
+    document.querySelector(".modal2").style.display = 'flex';
+    document.querySelector(".modal2 .win-m").innerText = who + ' Win!';
+}`
+
+* who Win function will be called in each (if win) situation it will select the first element that has moda12 as a class then it will change the property display from 'none' to be 'flex'. to make the pop window appears then it will select the text inside that window and change the text to (whatever it pass to it +" Win!").
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -5,43 +5,57 @@ let contentName1 = "Player1";
 
 let contentName2 = "Player2";
 
+//select player one and player two words in the header section to change them with players real names 
+let firstPlayer = document.querySelector(".player-one-section h3");
+let secondPlayer = document.querySelector(".player-two-section h3");
+
+firstPlayer.innerText = contentName1;
+secondPlayer.innerText = contentName2;
+
+
+
 //change the value of the vairables
 function changeName() {
     let nameinput1 = document.querySelector("#name-input1");
     let nameinput2 = document.querySelector("#name-input2");
 
-    contentName1 = nameinput1.value;
-    contentName2 = nameinput2.value;
+    // if users do not write their names it will not change to undefined it will still be player one and player two
+    if (nameinput1.value === "") {
+        console.log("it is empty");
+    } else {
+        contentName1 = nameinput1.value;
+        contentName2 = nameinput2.value;
+    }
 
-    console.log(contentName1);
-    console.log(contentName2);
     firstPlayer.innerText = contentName1;
     secondPlayer.innerText = contentName2;
 }
 
+
+
 let items = document.querySelectorAll(".items"); //to select the 9 zones 
 let turn = true; //to change turns x then o 
-let box = document.querySelector(".buttoncontainer"); //to select the start Over button 
+let box = document.querySelector(".buttoncontainer"); //to select the Play Again button 
 let counter = 0; //to count the clicks if the user clicks 9 times and no if statement excused then it will be a tie
-const newParagraph = document.createElement('p'); //to show win and tie messages or turn whom. 
+const newParagraph = document.createElement('p'); //to show win and tie messages or whom turns is. 
 const targetDiv = document.querySelector('.targetP'); //to select the position of the new paragraph
 let gameover = false; //to check if it true I will call resetAll function to stop the user from clicking anymore
 
-//player one WIN TIE LOSE 
+//player one WIN TIE LOSE data
 let player1Wins = 0;
 let player1Loses = 0;
 let player1Tie = 0;
 
-//player Two WIN TIE LOSE 
+//player Two WIN TIE LOSE data 
 let player2Wins = 0;
 let player2Loses = 0;
 let player2Tie = 0;
 
 
+
 function clickEvent() {
-    if (this.innerText === "") { //to check if it's empty it will allow him to click else it  will print this place is not available   
+    if (this.innerText === "") { //to check if it's empty it will allow him to click else it will console log this place is not available  
         counter += 1;
-        console.log(counter);
         if (turn === true) {
             this.innerText = "X";
             turn = false;
@@ -49,43 +63,36 @@ function clickEvent() {
             this.innerText = "O";
             turn = true;
         }
-        //after clicking, it will print whose turn is
+        //after clicking, it will print whose turn is 
         if (turn === true) {
             newParagraph.innerText = contentName1 + " turn";
 
         } else if (turn === false) {
             newParagraph.innerText = contentName2 + " turn";
         }
-        winLose()
+        winLose() //in every click, it will check the winlLoss function to see if there is a winner or not yet
     } else {
         console.log("this place is not available");
     }
 }
 
 function winLose() {
-    //check first row 
 
+    //check first row 
     if (items[0].innerText === items[1].innerText && items[1].innerText === items[2].innerText) {
         if (items[1].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
 
-            console.log(player1Wins);
-            console.log(player2Loses);
-
-            
         } else if (items[1].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
-
-            console.log(player2Wins);
-            console.log(player1Loses);
-            
-            
         }
     }
 
@@ -93,11 +100,13 @@ function winLose() {
     else if (items[3].innerText === items[4].innerText && items[4].innerText === items[5].innerText) {
         if (items[3].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[3].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -106,14 +115,16 @@ function winLose() {
 
     }
     //check third row 
-    else if (items[6].innerText === items[7].innerText && items[7].innerText === items[8].innerText) {
+    if (items[6].innerText === items[7].innerText && items[7].innerText === items[8].innerText) {
         if (items[6].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[6].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -125,11 +136,13 @@ function winLose() {
     else if (items[0].innerText === items[3].innerText && items[3].innerText === items[6].innerText) {
         if (items[0].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[0].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -141,11 +154,13 @@ function winLose() {
     else if (items[1].innerText === items[4].innerText && items[4].innerText === items[7].innerText) {
         if (items[1].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[1].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -158,12 +173,13 @@ function winLose() {
 
         if (items[2].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
-            winPar.innerText="X Win!"
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[2].innerText === "O") {
-            newParagraph.innerText = "O column third Win";
+            newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -174,11 +190,13 @@ function winLose() {
     else if (items[0].innerText === items[4].innerText && items[4].innerText === items[8].innerText) {
         if (items[0].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[0].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -189,11 +207,13 @@ function winLose() {
     else if (items[2].innerText === items[4].innerText && items[4].innerText === items[6].innerText) {
         if (items[2].innerText === "X") {
             newParagraph.innerText = contentName1 + ' Win!';
+            whoWin(contentName1);
             gameover = true;
             player1Wins += 1;
             player2Loses += 1;
         } else if (items[2].innerText === "O") {
             newParagraph.innerText = contentName2 + ' Win!';
+            whoWin(contentName2);
             gameover = true;
             player2Wins += 1;
             player1Loses += 1;
@@ -201,12 +221,15 @@ function winLose() {
 
     }
 
-    //tie 
-    else if (counter === 9) {
+    //tie > if number of click is 9 and gameover is false which means no if statment executed 
+    else if (counter === 9 && !gameover) {
         newParagraph.innerText = "Tie";
+        tiewindow();
         player1Tie += 1;
         player2Tie += 1;
     }
+
+    //if there is a winner it will stop him from clicking 
     if (gameover) {
         resetAll();
     }
@@ -222,67 +245,66 @@ function startOver() {
     gameover = false;
     newParagraph.innerText = "";
 
-    p1wins.innerText = "Win: "+player1Wins;
-    p1lose.innerText = "Lose: "+player1Loses;
-    p1tie.innerText ="Tie: "+player1Tie;
+    //to count how many wins and losses and tie for each player
+    p1wins.innerText = "Win: " + player1Wins;
+    p1lose.innerText = "Lose: " + player1Loses;
+    p1tie.innerText = "Tie: " + player1Tie;
 
 
-  p2wins.innerText = "Win: "+player2Wins;
-  p2lose.innerText = "Lose: "+player2Loses;
-  p2tie.innerText = "Tie: "+player2Tie;
+    p2wins.innerText = "Win: " + player2Wins;
+    p2lose.innerText = "Lose: " + player2Loses;
+    p2tie.innerText = "Tie: " + player2Tie;
 
 }
 
-//Stop the user from clicking after win function
+//Stop the user from clicking after winLoss function
 function resetAll() {
     for (let i = 0; i < items.length; i++) {
         items[i].removeEventListener('click', clickEvent);
     }
 }
 
-//Add Playing function  
+//Add Playing function enable the user to click x and o
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click', clickEvent);
 }
 //Add startOver function to box so when it's clicked it will allow the user to play again
 box.addEventListener('click', startOver);
 
-//to add the new newParagraph in this class 
+//to add the new newParagraph in this div 
 targetDiv.appendChild(newParagraph);
+
+
+
 
 
 //css
 
-
-document.querySelector('.close').addEventListener("click", function () {
-    document.querySelector('.modal').style.display = "none";
-});
-
+//select element with class close then add event listener click with function close so when X icon  clicks it will close the window  
+document.querySelector('.close').addEventListener("click", close);
 
 
 let closeButton = document.querySelector('.button');
 let closeButtonWin = document.querySelector('.modal2-content .button');
+let closeButtonTie = document.querySelector('.modal3-content .button');
 
 function close() {
     document.querySelector('.modal').style.display = "none";
     changeName();
 
-
 }
-function closeWin(){
+
+function closeWin() {
     document.querySelector('.modal2').style.display = "none";
+    document.querySelector('.modal3').style.display = "none";
 }
-closeButtonWin.addEventListener("click",closeWin);
 closeButton.addEventListener("click", close);
+closeButtonWin.addEventListener("click", closeWin);
+closeButtonTie.addEventListener("click", closeWin);
 
-
-//
-let winPar=document.querySelector(".win-m");
 
 
 //PlayerOne data 
-let firstPlayer = document.querySelector(".player-one-section h3");
-let secondPlayer = document.querySelector(".player-two-section h3");
 
 let p1wins = document.querySelector(".player-one-section .p1w");
 
@@ -299,3 +321,15 @@ let p2lose = document.querySelector(".player-two-section .p2L");
 
 let p2tie = document.querySelector(".player-two-section .p2t");
 
+
+//function who win when it call it will display a new window(second Windo) with the winner name 
+function whoWin(who) {
+    document.querySelector(".modal2").style.display = 'flex';
+    document.querySelector(".modal2 .win-m").innerText = who + ' Win!';;
+}
+
+//function  when it call it will display a new window(third window) with the tie massage 
+function tiewindow() {
+    document.querySelector(".modal3").style.display = 'flex';
+    document.querySelector(".modal3 .tie-m").innerText = ' Tie!';;
+}
